feab42587269d58b83739411ca5969526aa9ad4d
Vinicius <v1n1c1u5_1@hotmail.com>
2018-03-21 17:53:21 -0300

    Inicial

---
+++ app/src/main/java/com/vinicius/whatsapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.vinicius.whatsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference referenceFireBase = FirebaseDatabase.getInstance().getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        referenceFireBase.child("pontos").setValue(100);
+
+    }
+}
