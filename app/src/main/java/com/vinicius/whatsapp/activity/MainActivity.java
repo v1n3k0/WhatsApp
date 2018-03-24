fa406003f70f07dc4e8ca9c1d2a04fe0573bd868
Vinicius <v1n1c1u5_1@hotmail.com>
2018-03-24 11:11:26 -0300

    Tela Login

--- app/src/main/java/com/vinicius/whatsapp/activity/MainActivity.java
+++ app/src/main/java/com/vinicius/whatsapp/activity/MainActivity.java
@@ -1,10 +1,11 @@
-package com.vinicius.whatsapp;
+package com.vinicius.whatsapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.vinicius.whatsapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
