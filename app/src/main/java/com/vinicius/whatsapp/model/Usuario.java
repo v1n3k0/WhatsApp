b445dbd28bb6808b5868237d1fce34d847e4f13a
Vinicius <v1n1c1u5_1@hotmail.com>
2018-03-30 17:48:20 -0300

    Aula 186

--- app/src/main/java/com/vinicius/whatsapp/model/Usuario.java
+++ app/src/main/java/com/vinicius/whatsapp/model/Usuario.java
@@ -1,11 +1,25 @@
 package com.vinicius.whatsapp.model;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Exclude;
+import com.vinicius.whatsapp.config.ConfiguracaoFireBase;
+
 public class Usuario {
     private String id;
     private String nome;
     private String email;
     private String senha;
 
+    public Usuario(){
+
+    }
+
+    public void salvar(){
+        DatabaseReference referenceFirebase = ConfiguracaoFireBase.getFirebase();
+        referenceFirebase.child("usuarios").child(getId()).setValue(this);
+    }
+
+    @Exclude
     public String getId() {
         return id;
     }
@@ -30,6 +44,7 @@ public class Usuario {
         this.email = email;
     }
 
+    @Exclude
     public String getSenha() {
         return senha;
     }
