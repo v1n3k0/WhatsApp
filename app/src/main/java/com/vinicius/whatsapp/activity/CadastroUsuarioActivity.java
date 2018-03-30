254d842399f193a82a49b35b5f21aa3921868f52
Vinicius <v1n1c1u5_1@hotmail.com>
2018-03-30 17:31:16 -0300

    Aula 184

--- app/src/main/java/com/vinicius/whatsapp/activity/CadastroUsuarioActivity.java
+++ app/src/main/java/com/vinicius/whatsapp/activity/CadastroUsuarioActivity.java
@@ -1,12 +1,19 @@
 package com.vinicius.whatsapp.activity;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.vinicius.whatsapp.R;
+import com.vinicius.whatsapp.config.ConfiguracaoFireBase;
 import com.vinicius.whatsapp.model.Usuario;
 
 public class CadastroUsuarioActivity extends AppCompatActivity {
@@ -16,6 +23,8 @@ public class CadastroUsuarioActivity extends AppCompatActivity {
     private EditText senha;
     private Button botaoCadastrar;
     private Usuario usuario;
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +48,21 @@ public class CadastroUsuarioActivity extends AppCompatActivity {
         });
     }
 
+    //Salva o usuario
     public void cadastrarUsuario(){
-
+        autenticacao = ConfiguracaoFireBase.getFirebaseAutenticacao();
+        autenticacao.createUserWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+        ).addOnCompleteListener(CadastroUsuarioActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(CadastroUsuarioActivity.this, "Sucesso ao cadastrar usuário", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(CadastroUsuarioActivity.this, "Erro ao cadastrar usuário", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/vinicius/whatsapp/config/ConfiguracaoFireBase.java
+++ app/src/main/java/com/vinicius/whatsapp/config/ConfiguracaoFireBase.java
@@ -1,10 +1,12 @@
 package com.vinicius.whatsapp.config;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public final class ConfiguracaoFireBase {
     private static DatabaseReference referenceFireBase;
+    private static FirebaseAuth autenticacao;
 
     public static DatabaseReference getFirebase(){
         if(referenceFireBase == null){
@@ -12,6 +14,13 @@ public final class ConfiguracaoFireBase {
         }
 
         return referenceFireBase;
+    }
+
+    public static FirebaseAuth getFirebaseAutenticacao(){
+        if(autenticacao == null){
+            autenticacao = FirebaseAuth.getInstance();
+        }
+        return autenticacao;
     }
 
 }
