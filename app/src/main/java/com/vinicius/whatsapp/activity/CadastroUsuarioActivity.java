fe00564af710e593190709ff9859e660a1591be5
Vinicius <v1n1c1u5_1@hotmail.com>
2018-03-30 17:10:09 -0300

    Aula 183

--- app/src/main/java/com/vinicius/whatsapp/activity/CadastroUsuarioActivity.java
+++ app/src/main/java/com/vinicius/whatsapp/activity/CadastroUsuarioActivity.java
@@ -2,14 +2,44 @@ package com.vinicius.whatsapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.vinicius.whatsapp.R;
+import com.vinicius.whatsapp.model.Usuario;
 
 public class CadastroUsuarioActivity extends AppCompatActivity {
+
+    private EditText nome;
+    private EditText email;
+    private EditText senha;
+    private Button botaoCadastrar;
+    private Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_usuario);
+
+        nome = findViewById(R.id.edit_cadastro_nome);
+        email = findViewById(R.id.edit_cadastro_email);
+        senha = findViewById(R.id.edit_cadastrosenha);
+        botaoCadastrar = findViewById(R.id.button_cadastrar);
+
+        botaoCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                usuario = new Usuario();
+                usuario.setNome(nome.getText().toString());
+                usuario.setEmail(email.getText().toString());
+                usuario.setSenha(senha.getText().toString());
+                cadastrarUsuario();
+            }
+        });
+    }
+
+    public void cadastrarUsuario(){
+
     }
 }
