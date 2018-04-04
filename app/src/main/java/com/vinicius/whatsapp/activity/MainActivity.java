9699abcf499247a1f1a6dbabc34e0dc212de5e50
Vinicius <v1n1c1u5_1@hotmail.com>
2018-04-04 19:59:39 -0300

    Aula 196

--- app/src/main/java/com/vinicius/whatsapp/activity/MainActivity.java
+++ app/src/main/java/com/vinicius/whatsapp/activity/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,7 +18,6 @@ import com.vinicius.whatsapp.config.ConfiguracaoFireBase;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button botaoSair;
     private FirebaseAuth autenticacao;
     private Toolbar toolbar;
 
@@ -25,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        autenticacao = ConfiguracaoFireBase.getFirebaseAutenticacao();
 
         toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("WhatsApp");
@@ -38,5 +40,26 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.menu_main, menu);
 
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.action_sair:
+                deslogaUsuario();
+                return true;
+            case R.id.action_settings:
+                    return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void deslogaUsuario(){
+        autenticacao.signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
