e1ed57229f9a3a2d4ea93aa1f115651d0f325ff7
Vinicius <v1n1c1u5_1@hotmail.com>
2018-04-14 22:11:07 -0300

    Aula 216

--- app/src/main/java/com/vinicius/whatsapp/fragment/ContatosFragment.java
+++ app/src/main/java/com/vinicius/whatsapp/fragment/ContatosFragment.java
@@ -6,14 +6,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.vinicius.whatsapp.R;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ContatosFragment extends Fragment {
 
+    private ListView listView;
+    private ArrayAdapter adapter;
+    private ArrayList<String> contatos;
 
     public ContatosFragment() {
         // Required empty public constructor
@@ -23,8 +30,25 @@ public class ContatosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        //Instânciar objetos
+        contatos = new ArrayList<>();
+        contatos.add("Vinicius");
+        contatos.add("José");
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contatos, container, false);
+        View view = inflater.inflate(R.layout.fragment_contatos, container, false);
+
+        //Monta listView e adapter
+        listView = view.findViewById(R.id.lv_contatos);
+        adapter = new ArrayAdapter(
+                getActivity(),
+                R.layout.lista_contato,
+                contatos
+        );
+        listView.setAdapter(adapter);
+
+        return view;
     }
 
 }
